1627bb885ef3c3f65cff7cc17152e35bd888635d
ayj412 <ayj412@naver.com>
2019-05-25 08:58:06 +0000

    정책관리 구현

--- backend/djangoapps/policy/views.py
+++ backend/djangoapps/policy/views.py
@@ -17,6 +17,7 @@ def service(request):
         '''
         cur.execute(query)
         row = cur.fetchall()
+        print(row)
         en = row[0][0]
         ko = row[0][1]
         ja = row[0][2]
@@ -29,14 +30,228 @@ def service(request):
         }
     return render(request, 'policy/service.html', context)
 
+def api_service_edit1(request):
+    enSum = request.POST.get('enSum')
+
+    with connections['default'].cursor() as cur:
+        query = '''
+            update tbl_policy_manage
+            set en = '{enSum}', en_modify_date = now()
+            where type = 'S';
+            )
+        '''.format(
+            enSum = enSum
+        )
+        cur.execute(query)
+    return JsonResponse({'result': '200'})
+
+def api_service_edit2(request):
+    koSum = request.POST.get('koSum')
+
+    with connections['default'].cursor() as cur:
+        query = '''
+            update tbl_policy_manage
+            set ko = '{koSum}', ko_modify_date = now()
+            where type = 'S';
+            )
+        '''.format(
+            koSum = koSum
+        )
+        cur.execute(query)
+    return JsonResponse({'result': '200'})
+
+def api_service_edit3(request):
+    jaSum = request.POST.get('jaSum')
+
+    with connections['default'].cursor() as cur:
+        query = '''
+            update tbl_policy_manage
+            set ja = '{jaSum}', ja_modify_date = now()
+            where type = 'S';
+            )
+        '''.format(
+            jaSum = jaSum
+        )
+        cur.execute(query)
+    return JsonResponse({'result': '200'})
+
+def api_service_edit4(request):
+    zhSum = request.POST.get('zhSum')
+
+    with connections['default'].cursor() as cur:
+        query = '''
+            update tbl_policy_manage
+            set zh = '{zhSum}', zh_modify_date = now()
+            where type = 'S';
+            )
+        '''.format(
+            zhSum = zhSum
+        )
+        cur.execute(query)
+    return JsonResponse({'result': '200'})
 
 def privacy(request):
+    with connections['default'].cursor() as cur:
+        query = '''
+            select en, ko, ja, zh
+            from tbl_policy_manage
+            where type = 'P';
+        '''
+        cur.execute(query)
+        row = cur.fetchall()
+        print(row)
+        en = row[0][0]
+        ko = row[0][1]
+        ja = row[0][2]
+        zh = row[0][3]
+        context = {
+            'en': en,
+            'ko': ko,
+            'ja': ja,
+            'zh': zh
+        }
 
-    context = {}
     return render(request, 'policy/privacy.html', context)
 
+def api_privacy_edit1(request):
+    enSum = request.POST.get('enSum')
+
+    with connections['default'].cursor() as cur:
+        query = '''
+            update tbl_policy_manage
+            set en = '{enSum}', en_modify_date = now()
+            where type = 'P';
+            )
+        '''.format(
+            enSum = enSum
+        )
+        cur.execute(query)
+    return JsonResponse({'result': '200'})
+
+def api_privacy_edit2(request):
+    koSum = request.POST.get('koSum')
+
+    with connections['default'].cursor() as cur:
+        query = '''
+            update tbl_policy_manage
+            set ko = '{koSum}', ko_modify_date = now()
+            where type = 'P';
+            )
+        '''.format(
+            koSum = koSum
+        )
+        cur.execute(query)
+    return JsonResponse({'result': '200'})
+
+def api_privacy_edit3(request):
+    jaSum = request.POST.get('jaSum')
+
+    with connections['default'].cursor() as cur:
+        query = '''
+            update tbl_policy_manage
+            set ja = '{jaSum}', ja_modify_date = now()
+            where type = 'P';
+            )
+        '''.format(
+            jaSum = jaSum
+        )
+        cur.execute(query)
+    return JsonResponse({'result': '200'})
+
+def api_privacy_edit4(request):
+    zhSum = request.POST.get('zhSum')
+
+    with connections['default'].cursor() as cur:
+        query = '''
+            update tbl_policy_manage
+            set zh = '{zhSum}', zh_modify_date = now()
+            where type = 'P';
+            )
+        '''.format(
+            zhSum = zhSum
+        )
+        cur.execute(query)
+    return JsonResponse({'result': '200'})
 
 def refund(request):
-
-    context = {}
+    with connections['default'].cursor() as cur:
+        query = '''
+            select en, ko, ja, zh
+            from tbl_policy_manage
+            where type = 'R';
+        '''
+        cur.execute(query)
+        row = cur.fetchall()
+        print(row)
+        en = row[0][0]
+        ko = row[0][1]
+        ja = row[0][2]
+        zh = row[0][3]
+        context = {
+            'en': en,
+            'ko': ko,
+            'ja': ja,
+            'zh': zh
+        }
     return render(request, 'policy/refund.html', context)
+
+def api_refund_edit1(request):
+    enSum = request.POST.get('enSum')
+
+    with connections['default'].cursor() as cur:
+        query = '''
+            update tbl_policy_manage
+            set en = '{enSum}', en_modify_date = now()
+            where type = 'R';
+            )
+        '''.format(
+            enSum = enSum
+        )
+        cur.execute(query)
+    return JsonResponse({'result': '200'})
+
+def api_refund_edit2(request):
+    koSum = request.POST.get('koSum')
+
+    with connections['default'].cursor() as cur:
+        query = '''
+            update tbl_policy_manage
+            set ko = '{koSum}', ko_modify_date = now()
+            where type = 'R';
+            )
+        '''.format(
+            koSum = koSum
+        )
+        cur.execute(query)
+    return JsonResponse({'result': '200'})
+
+def api_refund_edit3(request):
+    jaSum = request.POST.get('jaSum')
+
+    with connections['default'].cursor() as cur:
+        query = '''
+            update tbl_policy_manage
+            set ja = '{jaSum}', ja_modify_date = now()
+            where type = 'R';
+            )
+        '''.format(
+            jaSum = jaSum
+        )
+        cur.execute(query)
+    return JsonResponse({'result': '200'})
+
+def api_refund_edit4(request):
+    zhSum = request.POST.get('zhSum')
+
+    with connections['default'].cursor() as cur:
+        query = '''
+            update tbl_policy_manage
+            set zh = '{zhSum}', zh_modify_date = now()
+            where type = 'R';
+            )
+        '''.format(
+            zhSum = zhSum
+        )
+        cur.execute(query)
+    return JsonResponse({'result': '200'})
+
