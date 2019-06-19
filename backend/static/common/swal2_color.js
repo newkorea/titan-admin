89d4266cc99bc60ef0cec663063b1400e9185c31
ayj412 <ayj412@naver.com>
2019-06-19 11:16:49 +0000

    swal_color 추가

---
+++ backend/static/common/swal2_color.js
@@ -0,0 +1,20 @@
+function swalColor(type){
+    if(type == 'success'){
+        return '#3fa556';
+    }
+    else if(type == 'error'){
+        return '#3fa556';
+    }
+    else if(type == 'warning'){
+        return '#facea8';
+    }
+    else if(type == 'info'){
+        return '#3fc3ee';
+    }
+    else if(type == 'question'){
+        return '#87adbd';
+    }
+    else{
+        return '#333333';
+    }
+}
